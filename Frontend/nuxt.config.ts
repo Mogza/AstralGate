6090b9aa0f3c2f1d11380cbffc72bf8d635010f8
Mogza <mael-mamadou.diagne@epitech.eu>
2024-10-09 11:36:58 +0200

    Add - Landing Page + Lights

--- Frontend/nuxt.config.ts
+++ Frontend/nuxt.config.ts
@@ -1,5 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  app: {
+    head: {
+      title: 'AstralGate',
+      meta: [
+        { name: 'description', content: 'Welcome to AstralGate!' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
+  css: [
+    '@/assets/css/main.css'
+  ],
 })
---
+++ Frontend/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="light lightPink"></div>
+    <div class="light lightBlue"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.light {
+  position: absolute;
+  border-radius: 50%;
+  opacity: 0.3;
+  filter: blur(100px);
+}
+
+.lightPink {
+  width: 2500px;
+  height: 700px;
+  background-color: rgba(194, 82, 225, 0.25);
+  top: -30%;
+  left: -10%;
+}
+.lightBlue {
+  width: 2500px;
+  height: 700px;
+  background-color: rgba(110, 203, 245, 0.25);
+  top: 25%;
+  left: -15%;
+}
+</style>
