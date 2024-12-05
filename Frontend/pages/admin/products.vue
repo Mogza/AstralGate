01fc16df12ca361287470865be2f564c1bea172e
Mogza <mael-mamadou.diagne@epitech.eu>
2024-12-05 13:19:16 +0100

    Fix - Content box size

--- Frontend/pages/admin/products.vue
+++ Frontend/pages/admin/products.vue
@@ -3,41 +3,44 @@
     <!-- Sidebar -->
     <AdminSideBar :current-tab=currentTab @tab-changed="handleTabChange" />
 
-    <!-- Title -->
-    <h1
-        class="text-transparent bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text font-bold text-5xl mt-10 ml-20"
-    >
-      Products
-    </h1>
-
-    <!-- ContentBox -->
-    <div class="flex-1 flex items-center mt-14 -ml-20">
-      <div
-          class="w-11/12 h-5/6 rounded-lg shadow-lg bg-gradient-to-r from-purple-400 to-blue-400 p-2 opacity-100"
+    <!-- Page Content -->
+    <div class="flex-1 flex flex-col">
+      <!-- Title -->
+      <h1
+          class="text-transparent bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text font-bold text-5xl mt-10 ml-20"
       >
-        <div class="w-full h-full bg-[#f4f3f2] rounded-lg p-4 overflow-y-auto">
-          <table class="table-auto w-full border-collapse border border-gray-300">
-            <thead>
-            <tr class="bg-gradient-to-r from-purple-400 to-blue-400 text-white">
-              <th class="border border-gray-300 px-4 py-2">User Name</th>
-              <th class="border border-gray-300 px-4 py-2">Title</th>
-              <th class="border border-gray-300 px-4 py-2">Description</th>
-              <th class="border border-gray-300 px-4 py-2">Price</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr
-                v-for="product in products"
-                :key="product.id"
-                class="text-center odd:bg-gray-100 even:bg-gray-200"
-            >
-              <td class="border border-gray-300 px-4 py-2">{{ product.user_name }}</td>
-              <td class="border border-gray-300 px-4 py-2">{{ product.title }}</td>
-              <td class="border border-gray-300 px-4 py-2">{{ product.description }}</td>
-              <td class="border border-gray-300 px-4 py-2">{{ product.usd_price }}$</td>
-            </tr>
-            </tbody>
-          </table>
+        Products
+      </h1>
+
+      <!-- ContentBox -->
+      <div class="flex-1 flex mt-14 ml-24">
+        <div
+            class="w-11/12 h-5/6 rounded-lg shadow-lg bg-gradient-to-r from-purple-400 to-blue-400 p-2 opacity-100"
+        >
+          <div class="w-full h-full bg-[#f4f3f2] rounded-lg p-4 overflow-y-auto">
+            <table class="table-auto w-full border-collapse border border-gray-300">
+              <thead>
+              <tr class="bg-gradient-to-r from-purple-400 to-blue-400 text-white">
+                <th class="border border-gray-300 px-4 py-2">User Name</th>
+                <th class="border border-gray-300 px-4 py-2">Title</th>
+                <th class="border border-gray-300 px-4 py-2">Description</th>
+                <th class="border border-gray-300 px-4 py-2">Price</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr
+                  v-for="product in products"
+                  :key="product.id"
+                  class="text-center odd:bg-gray-100 even:bg-gray-200"
+              >
+                <td class="border border-gray-300 px-4 py-2">{{ product.user_name }}</td>
+                <td class="border border-gray-300 px-4 py-2">{{ product.title }}</td>
+                <td class="border border-gray-300 px-4 py-2">{{ product.description }}</td>
+                <td class="border border-gray-300 px-4 py-2">{{ product.usd_price }}$</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
--- Frontend/pages/admin/users.vue
+++ Frontend/pages/admin/users.vue
@@ -3,43 +3,46 @@
     <!-- Sidebar -->
     <AdminSideBar :current-tab=currentTab @tab-changed="handleTabChange" />
 
-    <!-- Title -->
-    <h1
-        class="text-transparent bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text font-bold text-5xl mt-10 ml-20"
-    >
-      Users
-    </h1>
-
-    <!-- ContentBox -->
-    <div class="flex-1 flex items-center mt-14 -ml-20">
-      <div
-          class="w-11/12 h-5/6 rounded-lg shadow-lg bg-gradient-to-r from-purple-400 to-blue-400 p-2 opacity-100"
+    <!-- Page Content -->
+    <div class="flex-1 flex flex-col">
+      <!-- Title -->
+      <h1
+          class="text-transparent bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text font-bold text-5xl mt-10 ml-20"
       >
-        <div class="w-full h-full bg-[#f4f3f2] rounded-lg p-4 overflow-y-auto">
-          <table class="table-auto w-full border-collapse border border-gray-300">
-            <thead>
-            <tr class="bg-gradient-to-r from-purple-400 to-blue-400 text-white">
-              <th class="border border-gray-300 px-4 py-2">Username</th>
-              <th class="border border-gray-300 px-4 py-2">First Name</th>
-              <th class="border border-gray-300 px-4 py-2">Last Name</th>
-              <th class="border border-gray-300 px-4 py-2">Email</th>
-              <th class="border border-gray-300 px-4 py-2">Phone Number</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr
-                v-for="user in users"
-                :key="user.id"
-                class="text-center odd:bg-gray-100 even:bg-gray-200"
-            >
-              <td class="border border-gray-300 px-4 py-2">{{ user.username }}</td>
-              <td class="border border-gray-300 px-4 py-2">{{ user.first_name }}</td>
-              <td class="border border-gray-300 px-4 py-2">{{ user.last_name }}</td>
-              <td class="border border-gray-300 px-4 py-2">{{ user.email }}</td>
-              <td class="border border-gray-300 px-4 py-2">{{ user.phone_number }}</td>
-            </tr>
-            </tbody>
-          </table>
+        Users
+      </h1>
+
+      <!-- ContentBox -->
+      <div class="flex-1 flex mt-14 ml-24">
+        <div
+            class="w-11/12 h-5/6 rounded-lg shadow-lg bg-gradient-to-r from-purple-400 to-blue-400 p-2 opacity-100"
+        >
+          <div class="w-full h-full bg-[#f4f3f2] rounded-lg p-4 overflow-y-auto">
+            <table class="table-auto w-full border-collapse border border-gray-300">
+              <thead>
+              <tr class="bg-gradient-to-r from-purple-400 to-blue-400 text-white">
+                <th class="border border-gray-300 px-4 py-2">Username</th>
+                <th class="border border-gray-300 px-4 py-2">First Name</th>
+                <th class="border border-gray-300 px-4 py-2">Last Name</th>
+                <th class="border border-gray-300 px-4 py-2">Email</th>
+                <th class="border border-gray-300 px-4 py-2">Phone Number</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr
+                  v-for="user in users"
+                  :key="user.id"
+                  class="text-center odd:bg-gray-100 even:bg-gray-200"
+              >
+                <td class="border border-gray-300 px-4 py-2">{{ user.username }}</td>
+                <td class="border border-gray-300 px-4 py-2">{{ user.first_name }}</td>
+                <td class="border border-gray-300 px-4 py-2">{{ user.last_name }}</td>
+                <td class="border border-gray-300 px-4 py-2">{{ user.email }}</td>
+                <td class="border border-gray-300 px-4 py-2">{{ user.phone_number }}</td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
